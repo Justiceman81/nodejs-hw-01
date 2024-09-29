f7469fda4463e9b2a6e45959bd0e79f38f219cf4
Justiceman81 <Justiceman814@gmail.com>
2024-09-29 11:44:31 +0300

    added writeContacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -2,7 +2,7 @@ import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'node:fs/promises';
 
 export const writeContacts = async (updatedContacts) => {
-  await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts), {
+  await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2), {
     encoding: 'utf-8',
   });
 };
